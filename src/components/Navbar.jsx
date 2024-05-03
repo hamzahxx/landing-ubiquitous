d789d8b4723027685ccc8e61db9814fe4c963cce
Hamzah <hamzahchhapra2004@gmail.com>
2024-05-04 03:24:17 +0530

    Navbar Header
    - Added (Brand Name/Logo) Navbar Header in Navbar.jsx
    - Modified index.css for custom class named "navbar-header"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,11 @@
-function Navbar() {
-  return <></>;
+function Navbar(props) {
+  return (
+    <>
+      <div className="flex p-4">
+        <h1 className="navbar-header">{props.name}</h1>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
