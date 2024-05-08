bdcdff0fd46015eff6bf1debd8c1e46944f17b24
Hamzah <hamzahchhapra2004@gmail.com>
2024-05-08 11:51:05 +0530

    Hamburger Menu
    - Creation of HamburgerButton.css for writing animations
    - Wrote a fuctioning Hamburger Menu Button in Navbar.jsx
    - Modified index.css for fixing bugs created in making of the Hamburger Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+
+import "./HamburgerButton.css";
+
 function Navbar(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <div className="flex p-4">
         <h1 className="navbar-header">{props.name}</h1>
-        <ul className="nav-list">
+        <ul className={isOpen ? "left-0 top-16 nav-list" : "nav-list"}>
           <li className="nav-item">Home</li>
           <li className="nav-item">About</li>
           <li className="nav-item">Projects</li>
           <li className="nav-item">Contact</li>
           <li className="nav-item">Resume</li>
         </ul>
+        <button
+          className={`mt-[2px] ml-auto md:hidden hamburger-menu ${isOpen ? "active" : "inactive"}`}
+          onClick={toggleMenu}
+        >
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </button>
       </div>
     </>
   );
