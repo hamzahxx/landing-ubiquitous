f0b1725decf85e6f61b6856135cdbb526134f249
Hamzah <hamzahchhapra2004@gmail.com>
2024-05-11 20:05:37 +0530

    Bug Fix + Optimization
    - Bug fix of Navbar lowered when activated and screen size goes md+
    - Code Reduced by 16% without breaking functions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,28 @@
 import { useState } from "react";
-
 import "./HamburgerButton.css";
 
-function Navbar(props) {
+function Navbar({ name }) {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
   return (
-    <>
-      <div className="flex p-4">
-        <h1 className="navbar-header">{props.name}</h1>
-        <ul className={isOpen ? "left-0 top-16 nav-list" : "nav-list"}>
-          <li className="nav-item">Home</li>
-          <li className="nav-item">About</li>
-          <li className="nav-item">Projects</li>
-          <li className="nav-item">Contact</li>
-          <li className="nav-item">Resume</li>
-        </ul>
-        <button
-          className={`mt-[2px] ml-auto md:hidden hamburger-menu ${isOpen ? "active" : "inactive"}`}
-          onClick={toggleMenu}
-        >
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
-        </button>
-      </div>
-    </>
+    <div className="flex p-4">
+      <h1 className="navbar-header">{name}</h1>
+      <ul className={`nav-list md:top-0 ${isOpen ? "left-0 top-16" : ""}`}>
+        <li className="nav-item">Home</li>
+        <li className="nav-item">About</li>
+        <li className="nav-item">Projects</li>
+        <li className="nav-item">Contact</li>
+        <li className="nav-item">Resume</li>
+      </ul>
+      <button
+        className={`mt-[2px] ml-auto md:hidden hamburger-menu ${isOpen ? "active" : "inactive"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </button>
+    </div>
   );
 }
 
