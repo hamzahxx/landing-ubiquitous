388c35062bb1112534482664380c750e26ca4506
Hamzah <hamzahchhapra2004@gmail.com>
2024-06-21 03:24:24 +0530

    Feature + Bug Fix
    - Feature: On mobile devices the navbar-items will close the navbar-menu when clicked
    - Bug 1: hamburger button containing gaps fixed
    - Bug 2: the opened menu on mobile screens being larger then screen size fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ function Navbar({ name }) {
 
   return (
     <div className="flex p-4">
-      <h1 className="navbar-header">{name}</h1>
+      <h1 className="pl-3 font-sans font-bold text-3xl">{name}</h1>
       <ul className={`nav-list md:top-0 ${isOpen ? "left-0 top-16" : ""}`}>
-        <li className="nav-item">Home</li>
-        <li className="nav-item">About</li>
-        <li className="nav-item">Projects</li>
-        <li className="nav-item">Contact</li>
-        <li className="nav-item">Resume</li>
+        <li onClick={()=> setIsOpen(false)} className="nav-item">Home</li>
+        <li onClick={()=> setIsOpen(false)} className="nav-item">About</li>
+        <li onClick={()=> setIsOpen(false)} className="nav-item">Projects</li>
+        <li onClick={()=> setIsOpen(false)} className="nav-item">Contact</li>
+        <li onClick={()=> setIsOpen(false)} className="nav-item">Resume</li>
       </ul>
       <button
-        className={`mt-[2px] ml-auto md:hidden hamburger-menu ${isOpen ? "active" : "inactive"}`}
+        className={`mt-[2px] h-[100%] ml-auto md:hidden hamburger-menu ${isOpen ? "active" : "inactive"}`}
         onClick={() => setIsOpen(!isOpen)}
       >
         <div className="bar1"></div>
